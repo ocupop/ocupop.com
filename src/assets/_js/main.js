afcb1e5b79deb0c45174d6589280493ab6ccc9a4
Jon Burr <yojonathanwburr@gmail.com>
2018-11-28 15:54:58 -0800

    add barba, add example transition

--- src/assets/_js/main.js
+++ src/assets/_js/main.js
@@ -1,4 +1,4 @@
-import 'bootstrap';
+import "bootstrap";
 // Alternatively we can import features individually.
 // Also make sure to update the project.config.js if you are going to take this approach
 // import 'bootstrap/js/dist/util';
@@ -6,10 +6,80 @@ import 'bootstrap';
 
 // import App from './App';
 // import { TimelineMax, CSSPlugin, ScrollToPlugin, Draggable } from "gsap/all";
-import mixitup from 'mixitup';
+import mixitup from "mixitup";
+import Barba from "barba.js/dist/barba.js";
 
-const containerEl = document.getElementById('projectList')
+const containerEl = document.getElementById("projectList");
 
 if (containerEl) {
   var mixer = mixitup(containerEl);
 }
+
+Barba.Pjax.start();
+
+var FadeTransition = Barba.BaseTransition.extend({
+  start: function() {
+    /**
+     * This function is automatically called as soon the Transition starts
+     * this.newContainerLoading is a Promise for the loading of the new container
+     * (Barba.js also comes with an handy Promise polyfill!)
+     */
+    $(".footer-bar").animate({ height: "100%" }, 400);
+    // As soon the loading is finished and the old page is faded out, let's fade the new page
+    Promise.all([this.newContainerLoading, this.fadeOut()]).then(
+      this.fadeIn.bind(this)
+    );
+  },
+
+  fadeOut: function() {
+    /**
+     * this.oldContainer is the HTMLElement of the old Container
+     */
+
+    return $(this.oldContainer)
+      .animate({ opacity: 0 })
+      .promise();
+  },
+
+  fadeIn: function() {
+    /**
+     * this.newContainer is the HTMLElement of the new Container
+     * At this stage newContainer is on the DOM (inside our #barba-container and with visibility: hidden)
+     * Please note, newContainer is available just after newContainerLoading is resolved!
+     */
+
+    var _this = this;
+    var $el = $(this.newContainer);
+
+    $(this.oldContainer).hide();
+
+    $el.css({
+      visibility: "visible",
+      opacity: 0
+    });
+
+    $(".footer-bar").animate({ height: "4px" }, 400);
+
+    $el.animate({ opacity: 1 }, 500, function() {
+      /**
+       * Do not forget to call .done() as soon your transition is finished!
+       * .done() will automatically remove from the DOM the old Container
+       */
+
+      _this.done();
+    });
+  }
+});
+
+/**
+ * Next step, you have to tell Barba to use the new Transition
+ */
+
+Barba.Pjax.getTransition = function() {
+  /**
+   * Here you can use your own logic!
+   * For example you can use different Transition based on the current page or link...
+   */
+
+  return FadeTransition;
+};
