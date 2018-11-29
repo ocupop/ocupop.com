5993ccacda530a1a3ca692a6bc3f9b352bb1d429
Jon Burr <yojonathanwburr@gmail.com>
2018-11-29 13:47:14 -0800

    barba transition prototype

--- src/assets/_js/main.js
+++ src/assets/_js/main.js
@@ -8,109 +8,44 @@ import "bootstrap";
 // import { TimelineMax, CSSPlugin, ScrollToPlugin, Draggable } from "gsap/all";
 import mixitup from "mixitup";
 import Barba from "barba.js/dist/barba.js";
-
-const containerEl = document.getElementById("projectList");
-
-if (containerEl) {
-  var mixer = mixitup(containerEl);
-}
+import FirstTransition from "./transitions/FirstTransition";
+import SecondTransition from "./transitions/SecondTransition";
 
 Barba.Pjax.start();
-
-var Homepage = Barba.BaseView.extend({
-  namespace: "page",
-  onEnter: function() {
-    // The new Container is ready and attached to the DOM.
-    window.console.log("onEnter");
-    setTimeout(function() {
-      window.console.log("moving on");
-    }, 2000);
-  },
-  onEnterCompleted: function() {
-    // The Transition has just finished.
-    window.console.log("EnterCompleted");
-  },
-  onLeave: function() {
-    // A new Transition toward a new page has just started.
-    window.console.log("OnLeave");
-  },
-  onLeaveCompleted: function() {
-    // The Container has just been removed from the DOM.
-    window.console.log("onLeaveCompleted");
-  }
-});
-
-Barba.Dispatcher.on("linkClicked", function() {
-  //your listener
-  window.console.log("linkClicked");
-});
-
-// Don't forget to init the view!
-Homepage.init();
-
-var FadeTransition = Barba.BaseTransition.extend({
-  start: function() {
-    /**
-     * This function is automatically called as soon the Transition starts
-     * this.newContainerLoading is a Promise for the loading of the new container
-     * (Barba.js also comes with an handy Promise polyfill!)
-     */
-    $(".footer-bar").animate({ height: "100%" }, 400);
-    // As soon the loading is finished and the old page is faded out, let's fade the new page
-    Promise.all([this.newContainerLoading, this.fadeOut()]).then(
-      this.fadeIn.bind(this)
-    );
-  },
-
-  fadeOut: function() {
-    /**
-     * this.oldContainer is the HTMLElement of the old Container
-     */
-
-    return $(this.oldContainer)
-      .animate({ opacity: 0 })
-      .promise();
-  },
-
-  fadeIn: function() {
-    /**
-     * this.newContainer is the HTMLElement of the new Container
-     * At this stage newContainer is on the DOM (inside our #barba-container and with visibility: hidden)
-     * Please note, newContainer is available just after newContainerLoading is resolved!
-     */
-
-    var _this = this;
-    var $el = $(this.newContainer);
-
-    $(this.oldContainer).hide();
-
-    $el.css({
-      visibility: "visible",
-      opacity: 0
-    });
-
-    $(".footer-bar").animate({ height: "4px" }, 400);
-
-    $el.animate({ opacity: 1 }, 500, function() {
-      /**
-       * Do not forget to call .done() as soon your transition is finished!
-       * .done() will automatically remove from the DOM the old Container
-       */
-
-      _this.done();
-    });
-  }
-});
-
-/**
- * Next step, you have to tell Barba to use the new Transition
- */
 
 Barba.Pjax.getTransition = function() {
   /**
    * Here you can use your own logic!
    * For example you can use different Transition based on the current page or link...
    */
+  if (
+    lastClickEl !== undefined &&
+    lastClickEl.dataset.transition !== undefined
+  ) {
+    switch (lastClickEl.dataset.transition) {
+      case "FirstTransition":
+        return FirstTransition;
+        break;
+      case "SecondTransition":
+        return SecondTransition;
+        break;
+      default:
+        return FirstTransition;
+    }
+  }
 
-  return FadeTransition;
+  // if (FirstTransition.valid()) {
+  return FirstTransition;
+  // }
 };
+
+let lastClickEl;
+Barba.Dispatcher.on("linkClicked", el => {
+  lastClickEl = el;
+});
+
+const containerEl = document.getElementById("projectList");
+
+if (containerEl) {
+  var mixer = mixitup(containerEl);
+}
---
+++ src/assets/_js/transitions/FirstTransition.js
@@ -0,0 +1,47 @@
+import Barba from "barba.js/dist/barba.js";
+
+const FirstTransition = Barba.BaseTransition.extend({
+  start: function() {
+    console.log("FirstTransition");
+    Promise.all([this.newContainerLoading, this.transitionOut()]).then(
+      this.transitionIn.bind(this)
+    );
+  },
+  transitionOut: function() {
+    return $(this.oldContainer)
+      .animate({ opacity: 0 })
+      .promise();
+  },
+  transitionIn: function() {
+    var _this = this;
+    var $el = $(this.newContainer);
+
+    $(this.oldContainer).hide();
+
+    $el.css({
+      visibility: "visible",
+      opacity: 0
+    });
+
+    $el.animate({ opacity: 1 }, 400, function() {
+      /**
+       * Do not forget to call .done() as soon your transition is finished!
+       * .done() will automatically remove from the DOM the old Container
+       */
+
+      _this.done();
+    });
+  },
+
+  valid: function() {
+    var prev = Barba.HistoryManager.prevStatus();
+    var next = Barba.HistoryManager.currentStatus();
+
+    console.log(prev.namespace + " to " + JSON.stringify(next));
+
+    //return prev.namespace === "home" && next.namespace === "page";
+    return true;
+  }
+});
+
+export default FirstTransition;
