5547e2fe49486a632e15987bde4fbc894111eff3
Jon Burr <yojonathanwburr@gmail.com>
2019-02-25 15:51:42 -0800

    script load optimizations

--- src/assets/_js/main.js
+++ src/assets/_js/main.js
@@ -1,16 +1,6 @@
-import 'bootstrap'
-// Alternatively we can import features individually.
-// Also make sure to update the project.config.js if you are going to take this approach
-// import 'bootstrap/js/dist/util';
-// import 'bootstrap/js/dist/dropdown';
-
 import App from './App'
 // import { TimelineMax, CSSPlugin, ScrollToPlugin, Draggable } from "gsap/all";
-import mixitup from 'mixitup'
-import ScrollMagic from 'ScrollMagic'
-import TimelineMax from 'TimelineMax'
-import 'animation.gsap'
-import 'debug.addIndicators'
+
 import Barba from 'barba.js/dist/barba.js'
 import PageTransition from './transitions/PageTransition'
 import ProjectTransition from './transitions/ProjectTransition'
